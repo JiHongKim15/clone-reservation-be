7701035f7d25996335a9895968952b9f56e819bf
JiHongKim15 <15computer@hanmail.net>
2023-05-30 23:22:50 +0900

    create: 프로젝트 생성

---
+++ reserve/src/test/kotlin/com/clone/reserve/ReserveApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.clone.reserve
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReserveApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
